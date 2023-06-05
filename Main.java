08ba6f5fbdc82894ee004048614445b75a566bbb
iagompinheiro <iago.marcelino@icloud.com>
2023-06-05 19:54:25 -0300

    RendimentoPoupanca commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Rendimento rendimento = new Rendimento(8.5, 0.0821);
+        rendimento.interacaoUsuario();
+        rendimento.Resumo();
+
+    }
+}
---
+++ Rendimento.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Rendimento {
+    Scanner scanner = new Scanner(System.in);
+
+    private int valor;
+    private int quantMeses;
+    private double taxaSelic;
+    private double taxaRef;
+
+    public Rendimento(double taxaSelic, double taxaRef) {
+        this.taxaSelic = taxaSelic;
+        this.taxaRef = taxaRef;
+    }
+
+    public void interacaoUsuario() {
+
+        System.out.println("Digite o valor que você deseja investir:");
+         this.valor = scanner.nextInt();
+
+        System.out.println("Digite a quantidade de meses do rendimento:");
+        this.quantMeses = scanner.nextInt();
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQuantMeses() {
+        return quantMeses;
+    }
+
+    public double calculaRendimento() {
+        double rendimento;
+        if (taxaSelic < 8.5) {
+            rendimento = valor * (0.7 * taxaSelic + taxaRef) * quantMeses;
+        } else {
+            rendimento = valor * (0.005 + taxaRef) * quantMeses;
+        }
+        return rendimento;
+    }
+
+    public void Resumo() {
+        System.out.println("Valor inicial: " + valor);
+        System.out.println("Valor após o rendimento: " + calculaRendimento());
+    }
+}
