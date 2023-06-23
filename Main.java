5fc28a6f32790d27f8959b11565c614b018fadc5
Iago <130023096+iagompinheiro@users.noreply.github.com>
2023-06-22 23:53:26 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Rendimento rendimento = new Rendimento(8.5, 0.0821);
-        rendimento.interacaoUsuario();
+        Rendimento rendimento = new Rendimento();
         rendimento.Resumo();
-
     }
 }
